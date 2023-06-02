5a490ddb540ecbb537f6ea584410727bd7ed6613
Ткаченко Давоян <tkacheks03@mail.ru>
2023-06-02 23:45:11 +0700

    f

--- lab8.js
+++ lab8.js
@@ -40,6 +40,8 @@ function showDaysCount() {
     let inputDate = document.querySelector('input[type=date]');
     let birthday = new Date(inputDate.value);
     let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor (daysCount);
+    document.getElementById('dt').innerHTML = 'количество дней с даты рождения:' + daysCount;
     
 }
 function showTime() {
